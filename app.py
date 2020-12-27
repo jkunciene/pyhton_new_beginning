55f4ed0d0b9fe0548c644432211e302dc722515e
jkunciene <j.kunciene@gmail.com>
2020-12-27 17:06:52 +0200

    data type

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
-print('Jolita mokosi')
+# integer
 age = 37
+# perrasau reiksme
 age = 18
+# float
 price = 18.95
+# string
 first_name = "Jolita"
+# boolen
 is_online = True
+# turi buti vienodi duomenu tipai
+# todel paversiu i sting
 print(str(age)+"My age...")
+# duomenu paemimas
+name = input("What is yuor name?")
+print("Hello "+name)
+# turi buti vienodi duomenu tipai
+# todel paversiu i integer
+birth_year = input("Enter yuor birth year: ")
+age = 2020 -int(birth_year)
+print(age)
+
+# jei nezinau, koki duomenu tipa gaunu, visada galiu pasitikrinti
+print(type(name))
